c8dea3bc2aeca5f7a8341c132551d9931c63f82a
thiago <thi.henriquemaia@gmail.com>
2024-09-16 17:45:24 -0300

    tela de login

--- inteligencia_artificial/semanatextmining/aplicativoweb/inicio.py
+++ inteligencia_artificial/semanatextmining/aplicativoweb/inicio.py
@@ -7,15 +7,15 @@ app= Flask(__name__)# cria aplivativo flask]
 # / -> pagina principal
 # POST -> INSERIR
 # GET -> RECUPERAR
-@app.route('/',methods=['GET','POST'])
-def index():
+@app.route('/senia',methods=['GET','POST'])
+def abrir_assistente():
     audio_path= None
     # Pegar valor do html <textfield>
     if request.method== 'POST':
         texto= request.form['texto']
 
         # configurar o idioma
-        lingua='pt'
+        lingua='pt-br'
 
         # criação do objeto
 
@@ -28,7 +28,25 @@ def index():
 
         # Salvar arquivo
         tts.save(audio_path)
-    return render_template('index.html',audio=audio_path)
+    return render_template('senia.html',audio=audio_path)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET','POST'])
+def logar():
+    return render_template('login.html')
+
+@app.route('/autenticar',methods=['GET','POST'])
+def autentificar():
+    # mock
+    if request.method == 'POST':
+        if request.form['senha']=='123' and request.form['usuario']=='joao':
+            return render_template('senia.html')
+        else:
+            msg='Erro de autentificação'
+            return render_template('login.html',msg=msg)
 
 if __name__=='__main__':
     app.run(debug=True)
